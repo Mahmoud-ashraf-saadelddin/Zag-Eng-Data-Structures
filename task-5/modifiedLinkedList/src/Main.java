408ee191546231cd8ced950d31a08fad7518f046
Mahmoud-ashraf-saadelddin <149680591+Mahmoud-ashraf-saadelddin@users.noreply.github.com>
2023-12-07 18:29:35 +0200

    Add files via upload

---
+++ task-5/modifiedLinkedList/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        linkedTList n = new linkedTList();
+        n.addFirst("mm");
+        n.addFirst("aa");
+        n.addFirst("hh");
+        n.printElements();
+        n.reverse();
+        n.printElements();
+
+
+    }
+}
